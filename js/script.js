6a6c7e89b178b7b0dac53ca3efd4b7601e82df16
chanseya <rahley-aleksey@mail.ru>
2022-08-06 13:04:57 +0300

    completed catalog section

--- js/script.js
+++ js/script.js
@@ -25,9 +25,12 @@ const slider = tns({
     preventScrollOnTouch: 'auto',
     responsive: {
         320: {
-          nav: true,
-          autoWidth: true
+            nav: true,
+            autoWidth: true
         },
+        992: {
+            nav: false
+        }
     }
 });
   
@@ -38,3 +41,28 @@ document.querySelector('.next').addEventListener('click', function () {
 document.querySelector('.prev').addEventListener('click', function () {
     slider.goTo('prev');
 });
+
+(function($) {
+    $(function() {
+      
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+      
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        })
+    }
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+})(jQuery);
